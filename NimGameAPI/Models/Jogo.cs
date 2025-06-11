4ee0f5e33fc97c745f1536f3b07a68bd89941681
annaguerrra <annaguerracastro@gmail.com>
2025-06-11 16:36:40 -0300

    adicionando coisa

--- NimGameAPI/Models/Jogo.cs
+++ NimGameAPI/Models/Jogo.cs
@@ -5,12 +5,3 @@ public class Jogo
     public DateTime? DataFim { get; set; }
     public List<Movimento> Movimentos { get; set; }
 }
-
-public class Movimento
-{
-    public int Id { get; set; }
-    public int JogoId { get; set; }
-    public string Jogador { get; set; }
-    public int PalitosRemovidos { get; set; }
-    public DateTime DataMovimento { get; set; }
-}
